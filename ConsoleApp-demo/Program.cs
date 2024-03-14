f1e211cdcbcdc24e28be8ece510e280168fcd683
Nova Ridehed <nova.ridehed@skola.sala.se>
2024-03-14 13:29:17 +0100

    Fixade med kommentarer

--- ConsoleApp-demo/Program.cs
+++ ConsoleApp-demo/Program.cs
@@ -11,7 +11,6 @@
             int tal = rand.Next(1, 21);
             //Hur många gånger man har gissat
             
-
             string gissningstring = Console.ReadLine();
             //Gör om stringformat till ett tal
             int gissning = int.Parse(gissningstring);
@@ -20,40 +19,34 @@
             int gissningar = 0;
             for (; gissningar < 3; gissningar++)
             {
-
-
+                // Om man har gissat rätt
                 if (gissning == tal)
                 {
                     Console.WriteLine("Du har gissat rätt! Talet är " + tal + "!");
                     gissningar = 4;
                 }
+                //Om man har gissat för högt
                 else if (gissning > tal && gissningar <2)
                 {
                     Console.WriteLine("Lägre");
                 }
+                //Om man har gissat för lågt
                 else if (gissning < tal && gissningar < 2)
                 {
                     Console.WriteLine("Högre");
                 }
-             
-                
-
+                //Man får gissa igen
                 if(gissningar < 2)
                 {
                     Console.WriteLine("Gissa igen!");
                     gissningstring = Console.ReadLine();
                     gissning = int.Parse(gissningstring);
                 }
-                
-
             }
             if (gissningar == 3)
             {
                 Console.WriteLine("Du har gissat tre gånger. Talet var " + tal + "!");
             }
-            // gissning nummer två
-
-
         }
     }
 }
